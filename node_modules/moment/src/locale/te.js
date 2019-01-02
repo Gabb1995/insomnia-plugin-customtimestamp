26a4aa9d01f1a97556062b1f4b8adb843a816837
Gabriel Caruana <gabb1995@gmail.com>
2019-01-02 08:30:48 +0100

    moment.js update to version 2.23.0

--- node_modules/moment/src/locale/te.js
+++ node_modules/moment/src/locale/te.js
@@ -31,6 +31,7 @@ export default moment.defineLocale('te', {
         future : '%s లో',
         past : '%s క్రితం',
         s : 'కొన్ని క్షణాలు',
+        ss : '%d సెకన్లు',
         m : 'ఒక నిమిషం',
         mm : '%d నిమిషాలు',
         h : 'ఒక గంట',
@@ -74,6 +75,6 @@ export default moment.defineLocale('te', {
     },
     week : {
         dow : 0, // Sunday is the first day of the week.
-        doy : 6  // The week that contains Jan 1st is the first week of the year.
+        doy : 6  // The week that contains Jan 6th is the first week of the year.
     }
 });
